f94e36f8116f99264ae9ddb57afccb2333618892
alex-2by2 <137150612+alex-2by2@users.noreply.github.com>
2026-06-30 19:37:39 +0530

    Create GoodsIssue.js

---
+++ server/src/models/GoodsIssue.js
@@ -0,0 +1,131 @@
+// server/src/models/GoodsIssue.js
+
+import mongoose from "mongoose";
+
+const { Schema } = mongoose;
+
+const goodsIssueItemSchema = new Schema(
+  {
+    salesOrderItemId: {
+      type: Schema.Types.ObjectId,
+      required: true
+    },
+
+    item: {
+      type: Schema.Types.ObjectId,
+      ref: "Item",
+      required: true
+    },
+
+    orderedQuantity: {
+      type: Number,
+      required: true,
+      min: 0
+    },
+
+    issuedQuantity: {
+      type: Number,
+      required: true,
+      min: 0.000001
+    },
+
+    unitCost: {
+      type: Number,
+      required: true,
+      min: 0
+    },
+
+    lineTotal: {
+      type: Number,
+      required: true,
+      min: 0
+    }
+  },
+  {
+    _id: false
+  }
+);
+
+const goodsIssueSchema = new Schema(
+  {
+    company: {
+      type: Schema.Types.ObjectId,
+      ref: "Company",
+      required: true,
+      index: true
+    },
+
+    salesOrder: {
+      type: Schema.Types.ObjectId,
+      ref: "SalesOrder",
+      required: true,
+      index: true
+    },
+
+    warehouse: {
+      type: Schema.Types.ObjectId,
+      ref: "Warehouse",
+      required: true,
+      index: true
+    },
+
+    issueNumber: {
+      type: String,
+      required: true,
+      trim: true,
+      uppercase: true
+    },
+
+    issueDate: {
+      type: Date,
+      required: true
+    },
+
+    status: {
+      type: String,
+      enum: [
+        "DRAFT",
+        "POSTED",
+        "CANCELLED"
+      ],
+      default: "DRAFT",
+      index: true
+    },
+
+    items: {
+      type: [goodsIssueItemSchema],
+      default: []
+    },
+
+    totalAmount: {
+      type: Number,
+      default: 0,
+      min: 0
+    },
+
+    remarks: {
+      type: String,
+      trim: true,
+      default: ""
+    }
+  },
+  {
+    timestamps: true,
+    versionKey: false
+  }
+);
+
+goodsIssueSchema.index(
+  {
+    company: 1,
+    issueNumber: 1
+  },
+  {
+    unique: true
+  }
+);
+
+export default mongoose.model(
+  "GoodsIssue",
+  goodsIssueSchema
+);
