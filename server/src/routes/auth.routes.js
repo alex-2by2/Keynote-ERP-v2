3f9f27d25acecddd9c5abc310acdd3a1c2eb2cb8
alex-2by2 <137150612+alex-2by2@users.noreply.github.com>
2026-06-30 15:31:50 +0530

    Update auth.routes.js

--- server/src/routes/auth.routes.js
+++ server/src/routes/auth.routes.js
@@ -1,10 +1,22 @@
 // server/src/routes/auth.routes.js
 
 import { Router } from "express";
-import { login } from "../controllers/AuthController.js";
+
+import {
+  login,
+  logout
+} from "../controllers/AuthController.js";
+
+import authenticate from "../middleware/authenticate.js";
 
 const router = Router();
 
 router.post("/login", login);
+
+router.post(
+  "/logout",
+  authenticate,
+  logout
+);
 
 export default router;
