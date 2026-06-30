b31d8f54f8074b50c6acaab48993fd7b2699294b
alex-2by2 <137150612+alex-2by2@users.noreply.github.com>
2026-06-30 15:34:41 +0530

    Create Role.js

---
+++ server/src/models/Role.js
@@ -0,0 +1,55 @@
+// server/src/models/Role.js
+
+import mongoose from "mongoose";
+
+const { Schema } = mongoose;
+
+const roleSchema = new Schema(
+  {
+    code: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      uppercase: true,
+      index: true
+    },
+
+    name: {
+      type: String,
+      required: true,
+      trim: true
+    },
+
+    description: {
+      type: String,
+      trim: true,
+      default: ""
+    },
+
+    permissions: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Permission"
+      }
+    ],
+
+    system: {
+      type: Boolean,
+      default: false,
+      index: true
+    },
+
+    active: {
+      type: Boolean,
+      default: true,
+      index: true
+    }
+  },
+  {
+    timestamps: true,
+    versionKey: false
+  }
+);
+
+export default mongoose.model("Role", roleSchema);
