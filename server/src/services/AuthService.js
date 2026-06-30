659c5c79694cb302b9a9ffb8287c5e855a4f35d0
alex-2by2 <137150612+alex-2by2@users.noreply.github.com>
2026-06-30 15:23:17 +0530

    Create AuthService.js

---
+++ server/src/services/AuthService.js
@@ -0,0 +1,46 @@
+// server/src/services/AuthService.js
+
+import AppError from "../utils/AppError.js";
+import UserService from "./UserService.js";
+import { verifyPassword } from "../utils/password.js";
+import { generateAccessToken } from "../utils/jwt.js";
+
+export default class AuthService {
+  static async login(email, password) {
+    const user = await UserService.getByEmail(email);
+
+    if (!user) {
+      throw new AppError(
+        "Invalid email or password.",
+        401,
+        "INVALID_CREDENTIALS"
+      );
+    }
+
+    const passwordMatched = verifyPassword(password, user.password);
+
+    if (!passwordMatched) {
+      throw new AppError(
+        "Invalid email or password.",
+        401,
+        "INVALID_CREDENTIALS"
+      );
+    }
+
+    await UserService.updateLastLogin(user.id);
+
+    return {
+      accessToken: generateAccessToken({
+        sub: user.id,
+        role: user.role
+      }),
+      user: {
+        id: user.id,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        email: user.email,
+        role: user.role
+      }
+    };
+  }
+}
