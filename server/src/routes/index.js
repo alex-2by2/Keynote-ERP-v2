83d242b141145cc63624301a18716e07cb5d4276
alex-2by2 <137150612+alex-2by2@users.noreply.github.com>
2026-06-30 15:48:58 +0530

    Update index.js

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -7,14 +7,17 @@ import authRoutes from "./auth.routes.js";
 import userRoutes from "./user.routes.js";
 import roleRoutes from "./role.routes.js";
 import permissionRoutes from "./permission.routes.js";
+import departmentRoutes from "./department.routes.js";
 
 const router = Router();
 
 router.use("/health", healthRoutes);
+
 router.use("/auth", authRoutes);
 
 router.use("/users", userRoutes);
 router.use("/roles", roleRoutes);
 router.use("/permissions", permissionRoutes);
+router.use("/departments", departmentRoutes);
 
 export default router;
