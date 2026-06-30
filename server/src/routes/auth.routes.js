06be4ad00e90bfb8f277a222b21b255d172f510c
alex-2by2 <137150612+alex-2by2@users.noreply.github.com>
2026-06-30 15:20:58 +0530

    Create auth.routes.js

---
+++ server/src/routes/auth.routes.js
@@ -0,0 +1,10 @@
+// server/src/routes/auth.routes.js
+
+import { Router } from "express";
+import { login } from "../controllers/AuthController.js";
+
+const router = Router();
+
+router.post("/login", login);
+
+export default router;
