e3aa740477642b7abd6cea4873186aca8196fae3
alex-2by2 <137150612+alex-2by2@users.noreply.github.com>
2026-07-01 07:30:53 +0530

    Create Dashboard.jsx

---
+++ client/src/pages/dashboard/Dashboard.jsx
@@ -0,0 +1,13 @@
+// client/src/pages/dashboard/Dashboard.jsx
+
+export default function Dashboard() {
+  return (
+    <div className="dashboard-page">
+      <h1>Dashboard</h1>
+
+      <p>
+        Welcome to Keynote ERP.
+      </p>
+    </div>
+  );
+}
