dcaa16de948d1c8fe71b4aff9d736ccbccb32212
alex-2by2 <137150612+alex-2by2@users.noreply.github.com>
2026-07-01 07:31:30 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,38 @@
-import { Routes, Route } from "react-router-dom";
+// client/src/App.jsx
 
-function Home() {
-  return (
-    <main>
-      <h1>Keynote ERP v2</h1>
-    </main>
-  );
-}
+import {
+  Routes,
+  Route
+} from "react-router-dom";
+
+import Login from "./pages/auth/Login";
+import Dashboard from "./pages/dashboard/Dashboard";
+
+import AppLayout from "./layouts/AppLayout";
+
+import ProtectedRoute from "./routes/ProtectedRoute";
 
 export default function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route
+        path="/"
+        element={<Login />}
+      />
+
+      <Route
+        element={<ProtectedRoute />}
+      >
+        <Route
+          path="/app"
+          element={<AppLayout />}
+        >
+          <Route
+            index
+            element={<Dashboard />}
+          />
+        </Route>
+      </Route>
     </Routes>
   );
 }
