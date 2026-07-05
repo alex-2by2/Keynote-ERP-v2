eb928dc48a43a8b9bafb52c6023aa6b3d21d3888
alex-2by2 <137150612+alex-2by2@users.noreply.github.com>
2026-07-05 16:13:41 +0530

    Update SalesInvoice.js

--- server/src/models/SalesInvoice.js
+++ server/src/models/SalesInvoice.js
@@ -87,6 +87,12 @@ const salesInvoiceSchema = new Schema(
       default: null
     },
 
+    warehouse: {
+      type: Schema.Types.ObjectId,
+      ref: "Warehouse",
+      default: null
+    },
+
     voucher: {
       type: Schema.Types.ObjectId,
       ref: "Voucher",
